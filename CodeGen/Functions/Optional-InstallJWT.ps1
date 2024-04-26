db1ca67cfe3cf1e49527ca6d2862a7fde1b71ffc
Drag-NDrop <110815341+Drag-NDrop@users.noreply.github.com>
2024-04-26 20:07:17 +0200

    Update Optional-InstallJWT.ps1

--- CodeGen/Functions/Optional-InstallJWT.ps1
+++ CodeGen/Functions/Optional-InstallJWT.ps1
@@ -28,3 +28,67 @@ function ProcessJwtSettings {
         }
     }
 }
+
+$AddThisToAppSettingsFile = @"
+"Jwt": {
+    "Key": "$($JwtSettings[0].JwtKey)",
+    "Issuer": "$($JwtSettings[0].Issuer)"
+}
+"@
+
+$AddThisToProgramFileForSwaggerAuthSupport = @"
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
+"@
+
+$AddThisToProgramFile = @"
+//Jwt configuration starts here
+var jwtIssuer = builder.Configuration.GetSection("Jwt:Issuer").Get<string>();
+var jwtKey = builder.Configuration.GetSection("Jwt:Key").Get<string>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+ .AddJwtBearer(options =>
+ {
+     options.TokenValidationParameters = new TokenValidationParameters
+     {
+         ValidateIssuer = true,
+         ValidateAudience = true,
+         ValidateLifetime = true,
+         ValidateIssuerSigningKey = true,
+         ValidIssuer = jwtIssuer,
+         ValidAudience = jwtIssuer,
+         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+     };
+ });
+//Jwt configuration ends here
+"@
+# Add the dependencies if InstallJWT is set to true
+# Process the JwtSettings array
+# Add the credentials to the JwtSettings array in appsettings.json
+# Add the JWT middleware to the pipeline
+# Add the JWT authentication to the controllers
